2afc6272bee217be45d783f1bcdff643b9d1ac15
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-03-06 14:43:25 +0100

    fixed git errors

--- src/pages/home/EventCard.js
+++ src/pages/home/EventCard.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 const EventCard = ({img, place, startDate, endDate, title, author, alt}) => {
   return (
-    <article>
+    <article className='event-card'>
         <img src={img} alt={alt} />
-        <p>{place}</p>
-        <p>{`${startDate} - ${endDate}`}</p>
-        <h2>{title}</h2>
-        <h3>{`Af ${author}`}</h3>
-        <div className='buttons'>
-            <p>LÆS MERE</p>
-            <p>KØB BILLET</p>
-        </div>
+        <section className='card-info'>
+            <p>{place}</p>
+            <p className='date'>{`${startDate} - ${endDate}`}</p>
+            <h2>{title}</h2>
+            <h3>{`${author}`}</h3>
+            <div className='buttons'>
+                <p className='read'>LÆS MERE</p>
+                <p className='buy'>KØB BILLET</p>
+            </div>
+        </section>
+        
     </article>
   )
 }
