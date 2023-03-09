7c9e14d9e91a931e5bc4881d5cbff15938905a40
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-03-09 14:00:09 +0100

    add form

--- src/pages/buyTicket/BuyTicket.js
+++ src/pages/buyTicket/BuyTicket.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import { useEventsStore } from '../../stores/eventsStore'
+import { useEffect } from 'react'
 
-const BuyTicket = () => {
+const BuyTicket = ({id}) => {
+  const eventDetails = useEventsStore((state)=> state.eventDetails)
+  const fetchEventDetails = useEventsStore((state)=> state.fetchEventDetails)
+
+  useEffect(()=>{
+    fetchEventDetails(`https://api.mediehuset.net/detutroligeteater/events/${id}`)
+  },[])
+
   return (
-    <section className='buy-ticket'>
+    <>
+      {
+        eventDetails !== "" ? 
+          <section className='buy-ticket'>
+            <div className="ticket-header">
+              <img src={eventDetails.item.image} alt="ticket" />
+              <div className='info'>
+                <h1>Køb billet</h1>
+                <h2>{eventDetails.item.title}</h2>
+                <p className='place'>{`${eventDetails.item.stage_name} ${eventDetails.item.startdate} KL. ${eventDetails.item.starttime}`}</p>
+                <section className="form">
+                  <div>
+                    <p>FORNAVN</p>
+                    <input type="text" />
+                  </div>
+                  <div>
+                    <p>EFTERNAVN</p>
+                    <input type="text" />
+                  </div>
+                  <div className='double-field'>
+                    <p>VEJNAVN & NR</p>
+                    <input type="text" />
+                    <input type="text" />
+                  </div>
+                  <div>
+                    <p>POSTNR. & BY</p>
+                    <input type="text" />
+                    <input type="text" />
+                  </div>
+                </section>
+                <p>ALLE FELTER SKAL UDFYLDES</p>
+                <section>
+                  <p>ANTAL</p>
+                  <div>
+                    <p>-</p>
+                    <p>2</p>
+                    <p>+</p>
+                  </div>
+                  <div>
+                    <p>PRIS 1.000 DKK</p>
+                    <p>PRIS INKL  MOMS</p>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </section>
+          : null
+      }
 
-    </section>
+    </>
   )
 }
 
